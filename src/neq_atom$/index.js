1a87819865d52dddbc444ebd0954e47c9f5152dd
Brian Takita <brian.takita@gmail.com>
2022-02-11 22:49:54 -0600

    decorate_neq_atom$,decorate_neql_atom$: returns onSet()

--- src/neq_atom$/index.js
+++ src/neq_atom$/index.js
@@ -2,11 +2,12 @@ import { atom$ } from '../atom$/index.js'
 import { onSet } from '../index.js'
 /** @type {import('./index.d.ts').neq_atom$} */
 export const neq_atom$ = (initialValue)=>{
-	return decorate_neq_atom$(atom$(initialValue))
+	const $atom$ = atom$(initialValue)
+	decorate_neq_atom$($atom$)
+	return $atom$
 }
 export function decorate_neq_atom$($atom$) {
-	onSet($atom$, ({ newValue, abort })=>{
+	return onSet($atom$, ({ newValue, abort })=>{
 		if ($atom$.$ == newValue) abort()
 	})
-	return $atom$
 }
--- src/neql_atom$/index.js
+++ src/neql_atom$/index.js
@@ -2,11 +2,12 @@ import { atom$ } from '../atom$/index.js'
 import { onSet } from '../index.js'
 /** @type {import('./index.d.ts').neql_atom$} */
 export const neql_atom$ = (initialValue)=>{
-	return decorate_neql_atom$(atom$(initialValue))
+	const $atom$ = atom$(initialValue)
+	decorate_neql_atom$($atom$)
+	return $atom$
 }
 export function decorate_neql_atom$($atom$) {
-	onSet($atom$, ({ newValue, abort })=>{
+	return onSet($atom$, ({ newValue, abort })=>{
 		if ($atom$.$ === newValue) abort()
 	})
-	return $atom$
 }
