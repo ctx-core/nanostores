ab5788429f034efcab98d29cc7ebad8dea7ad69e
Brian Takita <brian.takita@gmail.com>
2022-03-12 21:24:26 -0600

    setter_computed$: return_payload$ atom takes a val instead of { store_val_a, val }

--- src/setter_computed$/index.js
+++ src/setter_computed$/index.js
@@ -5,12 +5,12 @@ export const setter_computed$ = (stores, cb)=>{
 	const isArray = Array.isArray(stores)
 	const store_a = isArray ? stores : [stores]
 	const store_val_a$ = computed(store_a, (...store_val_a)=>store_val_a)
-	const return_payload$ = atom({ store_val_a: [], val: undefined })
+	const return_payload$ = atom()
 	let _store_val_a = []
 	let run = store_val_a=>{
 		cb(isArray ? store_val_a : stores.get(), val=>{
 			if (store_val_a.every((store_val, $i)=>store_val === _store_val_a[$i])) {
-				return_payload$.set({ store_val_a, val })
+				return_payload$.set(val)
 			} else {
 				console.warn('setter_computed$|run|stale inputs', cb)
 			}
@@ -20,9 +20,7 @@ export const setter_computed$ = (stores, cb)=>{
 		_store_val_a = store_val_a
 		run(store_val_a)
 	})
-	const _setter_computed$ = computed$([return_payload$, store_val_a$], return_payload=>{
-		return return_payload.val
-	})
+	const _setter_computed$ = computed$([return_payload$, store_val_a$], val=>val)
 	_setter_computed$.store_a = store_a
 	return /** @type {import('./index.d.ts').SetterComputedAtom$} */_setter_computed$
 }
