c800fbb914e71ed374497d29879aa6a48cddbe0f
Brian Takita <brian.takita@gmail.com>
2023-02-07 02:26:01 -0500

    name convention updates

--- src/computed_/index.js
+++ src/computed_/index.js
@@ -1,6 +1,16 @@
 import { computed as _computed_ } from 'nanostores'
 import { readable_fn_ } from '../readable_fn_/index.js'
-export function computed_(stores, cb) {
+/**
+ *
+ * @param stores {AnyStore[]|Store}
+ * @param cb {((...values:StoreValues<OriginStores>)=>Value)|((value:StoreValue<OriginStore>)=>Value)}
+ * @returns {function(): *}
+ * @private
+ */
+export function computed_(
+	stores,
+	cb
+) {
 	return mix_computed_(_computed_(stores, cb))
 }
 export function mix_computed_(computed) {
